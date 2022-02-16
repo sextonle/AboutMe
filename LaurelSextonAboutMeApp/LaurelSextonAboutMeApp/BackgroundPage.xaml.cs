6ebd1efb0dd21f09d4e51b9498e9abc7afc99aa3
Laurel Sexton <sextonle@miamioh.edu>
2022-02-15 21:20:11 -0500

    Initial project set up

---
+++ LaurelSextonAboutMeApp/LaurelSextonAboutMeApp/BackgroundPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LaurelSextonAboutMeApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BackgroundPage : ContentPage
+    {
+        public BackgroundPage()
+        {
+            InitializeComponent();
+        }
+        //may be able to remove later
+        /*private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }*/
+ 
+       /* protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }*/
+    }
+}
---
+++ LaurelSextonAboutMeApp/LaurelSextonAboutMeApp/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LaurelSextonAboutMeApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : TabbedPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            //var navigationpage = new NavigationPage(new ViewPage());
+            //navigationpage.Title = "Background/ About Me";
+
+            Children.Add(new BackgroundPage());
+            //Children.Add(navigationpage);
+        }
+    }
+}
